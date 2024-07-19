7a967e95ad94c1218b568cccfe7419075265c5cc
Kevin Kusnardi <18548139+niko95kus@users.noreply.github.com>
2024-07-19 16:40:13 -0700

    update header: Authorization to Authorization1 to match frontend

--- src/main/java/com/doggo/csis3275_project_backend/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/doggo/csis3275_project_backend/configs/JwtAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
+        final String authHeader = request.getHeader("Authorization1");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
