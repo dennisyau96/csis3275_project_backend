62112d63835e7c10d4ef5e0e2e0d74986745ce53
chikityin <>
2024-07-23 17:59:05 -0700

    Transaction api finished showBill pay

--- src/main/java/com/doggo/csis3275_project_backend/Entities/Transaction.java
+++ src/main/java/com/doggo/csis3275_project_backend/Entities/Transaction.java
@@ -2,15 +2,26 @@ package com.doggo.csis3275_project_backend.Entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document
 public class Transaction {
     @Field(targetType = FieldType.OBJECT_ID)
     private String _id;
+    @Field(targetType = FieldType.OBJECT_ID)
+    private String booking_id;
+    @Field(targetType = FieldType.OBJECT_ID)
+    private String renter_id;
+    private Double price;
+    private Double tax;
+    private Double total_amount;
+    private String complete_time;
+    private Boolean is_paid;
 
 }
--- src/main/java/com/doggo/csis3275_project_backend/Web/TransactionController.java
+++ src/main/java/com/doggo/csis3275_project_backend/Web/TransactionController.java
@@ -1,4 +1,29 @@
 package com.doggo.csis3275_project_backend.Web;
 
+import com.doggo.csis3275_project_backend.Entities.GenericResponse;
+import com.doggo.csis3275_project_backend.Services.TransactionService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
 public class TransactionController {
+    private final TransactionService transactionService;
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping(path = "/showBill")
+    public GenericResponse showBill(@RequestHeader (name="Authorization") String rawToken,@RequestBody Map<String, Object> json, HttpServletResponse response){
+        return transactionService.showBill(rawToken, json,response);
+    }
+    @PostMapping(path = "/pay")
+    public GenericResponse pay(@RequestHeader (name="Authorization") String rawToken,@RequestBody Map<String, Object> json, HttpServletResponse response){
+        return transactionService.pay(rawToken,json);
+    }
 }
